8e998cb3707a8d3b3b8a5010fb54aef1ffae27c3
Jenny Sahng <jenny.s@hotmail.co.nz>
2018-03-03 05:44:37 +1300

    Tidy code

--- src/components/sidebar/index.js
+++ src/components/sidebar/index.js
@@ -1,43 +1,44 @@
-import { h, Component } from 'preact';
+import { h } from 'preact';
 // import { Link } from 'preact-router/match';
-
 import Filters from '../filters';
 import Pill from '../pill';
 import SocialLinks from '../socialLinks';
+import profilePic from '../../assets/profile_drawing.png';
 import style from './style';
 
-import profile_pic from '../../assets/profile_drawing.png';
-
-export default class Sidebar extends Component {
-	render() {
-		return (
-			<div class={style["sidebar-container"]}>
-				<div class={style.sidebar}>
-					<img
-						src={profile_pic}
-						class={style.profile_pic}
-						alt="Hi, my name's Jenny, and I like making things."
+const Sidebar = props => {
+	return (
+		<div class={style['sidebar-container']}>
+			<div class={style.sidebar}>
+				<img
+					src={profilePic}
+					class={style['profile-pic']}
+					alt="Hi, my name's Jenny, and I like making things."
+				/>
+				<header>
+					<h1>Hi, I'm Jenny.</h1>
+					<h3>I like front-end development, data, doodling &amp; dogs.</h3>
+				</header>
+				<nav>
+					{/* <Link activeClassName={style.active} href="/">
+						Home
+					</Link>
+					<Link activeClassName={style.active} href="/about">
+						About
+					</Link> */}
+					<Pill text="Home" />
+					<Pill
+						text="i"
+						className="pill-about"
+						color="dark-blue"
 						onClick={props.handleNavigation}
 					/>
-					<header>
-						<h1>Hi, I'm Jenny.</h1>
-						<h3>I like front-end development, data, doodling &amp; dogs.</h3>
-					</header>
-					<nav>
-						{/* <Link activeClassName={style.active} href="/">
-							Home
-						</Link>
-						<Link activeClassName={style.active} href="/about">
-							About
-						</Link> */}
-						<Pill text="Home" />
-						<Pill text="i" className="pill-about" />
-					</nav>
-					<Filters />
-				</div>
+				</nav>
 				<Filters onClick={props.handleFilter} />
 				<SocialLinks />
 			</div>
-		);
-	}
-}
+		</div>
+	);
+};
+
+export default Sidebar;
